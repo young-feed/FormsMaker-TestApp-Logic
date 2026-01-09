88d0568c339724f7092b4bff664eac796176b6cd
young-feed <danilovdanyaivanovi4@gmail.com>
2026-01-09 19:04:26 +0300

    реализация управления бд

--- dbmanager.hpp
+++ dbmanager.hpp
@@ -4,4 +4,20 @@
 #include <stdexcept>
 
 class DBManager {
+    PGconn* conn;
+public:
+    DBManager(const std::string& conninfo) {
+        conn = PQconnectdb(conninfo.c_str());
+        if (PQstatus(conn) != CONNECTION_OK) {
+            throw std::runtime_error(PQerrorMessage(conn));
+        }
+    }
+    ~DBManager() { PQfinish(conn); }
+
+    PGconn* get_conn() { return conn; }
+
+    // Вспомогательная функция для выполнения запросов
+    PGresult* query(const std::string& sql, const std::vector<const char*>& params) {
+        return PQexecParams(conn, sql.c_str(), params.size(), NULL, params.data(), NULL, NULL, 0);
+    }
 };
