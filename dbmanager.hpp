5dcfd6c5f3fadea1889f3faabe570aaea93c26e5
young-feed <danilovdanyaivanovi4@gmail.com>
2026-01-09 21:58:13 +0300

    Update dbmanager.hpp

--- dbmanager.hpp
+++ dbmanager.hpp
@@ -20,4 +20,21 @@ public:
     PGresult* query(const std::string& sql, const std::vector<const char*>& params) {
         return PQexecParams(conn, sql.c_str(), params.size(), NULL, params.data(), NULL, NULL, 0);
     }
+
+    bool soft_delete_course(int course_id) {
+    std::string id_str = std::to_string(course_id);
+    const char* params[1] = { id_str.c_str() };
+    
+    // SQL запрос: вместо DELETE используем UPDATE
+    PGresult* res = query("UPDATE courses SET is_deleted = true WHERE id = $1", {params[0]});
+    
+    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+        std::cerr << "Update failed: " << PQerrorMessage(conn) << std::endl;
+        PQclear(res);
+        return false;
+    }
+    
+    PQclear(res);
+    return true;
+}
 };
